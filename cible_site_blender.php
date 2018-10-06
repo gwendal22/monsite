84f6f8a5fbac7a433acb1c38d7718a9d646e30d5
gwendal22 <gwendal.richomme@gmail.com>
2018-10-06 17:42:44 +0200

    First upload of the project
    Signed-off-by: gwendal22 <gwendal.richomme@gmail.com>

---
+++ cible_site_blender.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Gwendal Richomme</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="Breizh_Dalek" content="SitePoint">
+
+  <link rel="stylesheet" href="Site.css?v=1.0">
+
+</head>
+
+<body>
+  <p>Dadoudidou</p>
+
+  <p>Ton nom est <<?php echo $_POST['Name']; ?></p> 
+</body>
+
+
+</html>
